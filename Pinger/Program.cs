898765cce56f8c4ad8c46197261130857cf10aab
Lucas Alves <lucasalves94@gmail.com>
2023-02-09 11:29:15 -0300

    added response, roundtrip, time to live and buffer size to the response

--- Pinger/Program.cs
+++ Pinger/Program.cs
@@ -15,5 +15,8 @@ string address = "4.2.2.2";
 PingReply reply = pingSender.Send(address, timeout, buffer, options);
 if (reply.Status == IPStatus.Success)
 {
-    Console.WriteLine("Success");
+    Console.WriteLine("Response: {0}", reply.Status.ToString());
+    Console.WriteLine("RoundTrip: {0}", reply.RoundtripTime);
+    Console.WriteLine("Time to live: {0}", reply.Options.Ttl);
+    Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
 }
